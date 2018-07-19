1223acb758b41d16dd46631a81a300a4529da1f3
Anuj Das <anujdas@gmail.com>
2018-07-19 14:40:44 -0700

    Make recursive types work using structs

--- src/cryogen/chest.cr
+++ src/cryogen/chest.cr
@@ -5,69 +5,61 @@ require "./crypto"
 
 module Cryogen
   class Chest
-    alias ChestObject = String | Hash(String, ChestObject)
-    alias ChestDocument = Hash(String, ChestObject)
-
-    class Entry
-      def initialize(@value : ChestObject, @prefix : String = "")
-      end
-
-      def decrypt(key : Key) : Entry
-        encrypted_value = @value
-        decrypted_value =
-          if encrypted_value.is_a?(Hash)
-            encrypted_value.
-              each_with_object({} of String => Entry) do |(nested_prefix, encrypted_entry), h|
-                h[nested_prefix] = Entry.new(encrypted_entry, nested_prefix).decrypt(key)
-              end
-          else
-            Crypto.verify_and_decrypt(encrypted_value, key)
-          end
-
-        Entry.new(decrypted_value, @prefix)
-      end
-
-      def encrypt(key : Key) : Entry
-        decrypted_value = @value # apparently the typechecker needs this
-        encrypted_value =
-          if decrypted_value.is_a?(Hash)
-            decrypted_value.
-              each_with_object({} of String => Entry) do |(nested_prefix, entry), h|
-                h[nested_prefix] = Entry.new(entry, nested_prefix).encrypt(key)
-              end
-          else
-            Crypto.encrypt_and_sign(decrypted_value, key)
-          end
-
-        Entry.new(encrypted_value, @prefix)
-      end
-    end
+    record EncryptedEntry, value : Nil | String | Hash(String, EncryptedEntry)
+    record Entry, value : Nil | String | Hash(String, Entry)
 
     def initialize(@chest_file : String, @key : Key)
     end
 
-    def decrypted_contents : ChestDocument
-      Entry.new(read_raw_contents).decrypt(@key)
+    def decrypted_contents : Entry
+      decrypt(read_raw_contents)
     end
 
-    def update_contents(new_contents : ChestDocument)
-      raw_contents = Entry.new(new_contents).encrypt(@key)
-      write_raw_contents(new_contents)
+    def update_contents!(new_contents : Entry)
+      write_raw_contents(encrypt(new_contents))
     end
 
-    private def parse_yaml(yaml_hash) : ChestObject
-      yaml_hash.each_with_object({} of String => ChestObject) do |(key, val), h|
-        h[key.as_s] = val.as_s? ? val.as_s : parse_yaml(val.as_h)
-      end
-    end
-
-    private def read_raw_contents : ChestDocument
+    private def read_raw_contents : EncryptedEntry
       raw_contents = File.open(@chest_file, "r") { |f| YAML.parse(f) }
-      parse_yaml(raw_contents.as_h).as(ChestDocument)
+      parse_yaml(raw_contents.as_h)
     end
 
-    private def write_raw_contents(raw_contents : ChestDocument)
+    private def write_raw_contents(raw_contents : EncryptedEntry)
       File.open(@chest_file, "w") { |f| YAML.dump(raw_contents, f) }
+    end
+
+    private def parse_yaml(yaml_hash : Hash) : EncryptedEntry
+      hash = yaml_hash.each_with_object({} of String => EncryptedEntry) do |(key, val), h|
+        h[key.as_s] = val.as_h? ? parse_yaml(val.as_h) : EncryptedEntry.new(val.as_s)
+      end
+
+      EncryptedEntry.new(hash)
+    end
+
+    private def decrypt(encrypted_entry : EncryptedEntry) : Entry
+      decrypted_value = 
+        case value = encrypted_entry.value
+        when String
+          Crypto.verify_and_decrypt(value, @key)
+        when Hash
+          value.each_with_object({} of String => Entry) { |(key, val), h| h[key] = decrypt(val) }
+        end
+
+      Entry.new(decrypted_value)
+    end
+
+    private def encrypt(decrypted_entry : Entry) : EncryptedEntry
+      encrypted_value = 
+        case value = decrypted_entry.value
+        when String
+          Crypto.encrypt_and_sign(value, @key)
+        when Hash
+          value.each_with_object({} of String => EncryptedEntry) do |(key, val), h|
+            h[key] = encrypt(val)
+          end
+        end
+
+      EncryptedEntry.new(encrypted_value)
     end
   end
 end
