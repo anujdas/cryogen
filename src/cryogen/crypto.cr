856442816030eac093dfd14b2281dda18d9548a4
Anuj Das <anujdas@gmail.com>
2018-07-20 16:22:01 -0700

    Fix encryption round-tripping

--- src/cryogen/crypto.cr
+++ src/cryogen/crypto.cr
@@ -12,18 +12,17 @@ module Cryogen
     SEPARATOR = '$'
 
     def self.encrypt_and_sign(value : String, key : Key) : String
-      # encrypt using 256-bit AES CBC
+      # set up cipher in encrypt mode using weird openssl-isms
       cipher = OpenSSL::Cipher.new(CIPHER_MODE)
       cipher.encrypt
       cipher.key = key.cipher_key
-      iv = cipher.random_iv.to_s
-      io = IO::Memory.new
-      io.write(cipher.update(value))
-      io.write(cipher.final)
-      data = io.to_slice.hexstring
+      iv = cipher.random_iv # let OpenSSL pick a random initialisation vector
+
+      # encrypt using 256-bit AES CBC
+      data = concat_bytes(cipher.update(value.to_slice), cipher.final)
 
       # sign (iv + data) using SHA256-HMAC
-      sig = OpenSSL::HMAC.digest(SIG_MODE, key.signing_key, iv + data)
+      sig = OpenSSL::HMAC.digest(SIG_MODE, key.signing_key, concat_bytes(iv, data))
 
       # return "iv$data$sig"
       [iv, data, sig].map { |s| Base64.strict_encode(s) }.join(SEPARATOR)
@@ -31,24 +30,28 @@ module Cryogen
 
     def self.verify_and_decrypt(encrypted_value : String, key : Key) : String
       # parse "iv$data$sig"
-      iv, encrypted_string, sig = encrypted_value.
-        split(SEPARATOR).map { |s| Base64.decode_string(s) }
-
+      iv, data, sig = encrypted_value.split(SEPARATOR).map { |s| Base64.decode(s) }
+        
       # verify HMAC-256 before decoding
-      calculated_sig = OpenSSL::HMAC.digest(SIG_MODE, key.signing_key, iv + encrypted_string)
+      calculated_sig = OpenSSL::HMAC.digest(SIG_MODE, key.signing_key, concat_bytes(iv, data))
       raise Error::SignatureInvalid.new unless calculated_sig == sig
 
-      # decode using AES-256-CBC
+      # set up cipher in decrypt mode using weird openssl-isms
       cipher = OpenSSL::Cipher.new(CIPHER_MODE)
       cipher.decrypt
       cipher.key = key.cipher_key
       cipher.iv = iv
-      IO::Memory.new.tap do |io|
-        io.write(cipher.update(encrypted_string))
-        io.write(cipher.final)
-      end.to_s
+
+      # decode using AES-256-CBC
+      String.new(concat_bytes(cipher.update(data), cipher.final))
     rescue e : OpenSSL::Cipher::Error
       raise Error::DecryptionError.new(e)
+    end
+
+    private def self.concat_bytes(*bytestrings : Bytes) : Bytes
+      io = IO::Memory.new
+      bytestrings.each { |bs| io.write(bs) }
+      io.to_slice
     end
   end
 end
