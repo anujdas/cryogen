e84fe11f7b1945de968007cb3fc245bef1169d3f
Anuj Das <anujdas@gmail.com>
2018-07-23 16:24:35 -0700

    Small fix to doc

--- src/cryogen/cli.cr
+++ src/cryogen/cli.cr
@@ -13,7 +13,7 @@ module Cryogen
       register_sub_command show : CLI::Show, description: "Show decrypted contents of vault"
       register_sub_command export : CLI::Export,
         description: "Print decrypted vault contents in shell `eval`-able ENV format"
-      register_sub_command edit : CLI::Edit, description: "Opens the vault in $EDITOR"
+      register_sub_command edit : CLI::Edit, description: "Open the vault in $EDITOR"
       register_sub_command unlock : CLI::Unlock, description: "Unlock the vault"
       register_sub_command lock : CLI::Lock, description: "Lock the vault"
       register_sub_command rekey : CLI::Rekey, description: "Rotate the vault key"
