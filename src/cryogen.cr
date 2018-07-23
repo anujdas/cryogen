cef01200e866182f84625799162f047432198ade
Anuj Das <anujdas@gmail.com>
2018-07-23 11:54:56 -0700

    Change key filename
    Before initial release, so compatibility isn't an issue

--- src/cryogen.cr
+++ src/cryogen.cr
@@ -3,7 +3,7 @@ require "./cryogen/*"
 module Cryogen
   BASE_DIR = ".cryogen"
   VAULT_FILE = "#{BASE_DIR}/vault.yml"
-  KEY_FILE = "#{BASE_DIR}/secret_key"
+  KEY_FILE = "#{BASE_DIR}/secret.key"
 end
 
 Cryogen::CLI::Main.run
